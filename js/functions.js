249d4cb19ca8d7e85ff6f927739a5d0ff5995c9c
Bruno Meneses <brunomenesest@gmail.com>
2021-07-22 00:34:13 -0300

    Meu portfolio em construção

---
+++ js/functions.js
@@ -0,0 +1,57 @@
+$(function(){
+
+    /*MENU MOBILE*/ 
+    $('.mobile').click(function(){
+        $(this).find('ul').slideToggle();
+    })
+    
+    /*menu scroll*/ 
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html, body').animate({'scrollTop':offSetTop});
+        
+        return false;
+    })
+    
+    //hover social-box    
+    $('.icon-wraper').hover(function(){
+        $(this).find('i').css("color", "#231B1B");
+    })
+
+    $('.icon-wraper').mouseleave(function(){
+        $(this).find('i').css("color", "#0D0D0");
+    })
+
+    $('footer .icon-wraper').hover(function(){
+        $(this).find('i').css("color", "#231B1B");
+    })
+
+    $('footer .icon-wraper').mouseleave(function(){
+        $(this).find('i').css("color", "#fff");
+    })
+
+    //hover portfolio
+    $('.port-box').hover(function(){
+        $(this).find('#soon').hide();
+    })
+
+    $('.port-box').mouseleave(function(){
+        $(this).find('#soon').show();
+    })
+
+    //CORREÇÃO MENU MOBILE
+    // $('.ul-mobile-a').click(function(){
+    //     $(this).find('.mobile').css('opacity', '0');
+    // })
+
+    //SOCIAL 
+    $('.img-box').hover(function(){
+        $(this).find('.social-thing').css('display', 'block');
+    })
+
+    $('.img-box').mouseleave(function(){
+        $(this).find('.social-thing').css('display', 'none');
+    })
+});
